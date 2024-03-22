d5d92a1c60f72fc1c2235a4639eef69c3c4bd93f
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-03-21 19:09:51 -0700

    adding middleware

--- app.js
+++ app.js
@@ -5,10 +5,42 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const handlebars = require('express-handlebars');
+
+const hbs = handlebars.create({
+  helpers: {
+    foo() {
+      return 'FOO!';
+    },
+    bar() {
+      return 'BAR!';
+    },
+  },
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Routes
+
+app.get('/', (req, res) => {
+  // set the url of the gif
+  const gifUrl =
+    'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl });
+});
+
 app.get('/:username', (req, res) => {
   res.send('Hello Squirrel');
+});
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
 });
 // Start Server
 
